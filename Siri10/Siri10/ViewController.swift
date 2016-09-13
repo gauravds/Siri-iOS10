f58e963d1f87f2cd60e05ba7ed3dffb07244bbda
GauravDS <gaurav.sharma@punchh.com>
2016-09-13 20:33:45 +0530

    Initail code added

---
+++ Siri10/Siri10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Siri10
+//
+//  Created by Gaurav Sharma on 13/09/16.
+//  Copyright © 2016 GDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
